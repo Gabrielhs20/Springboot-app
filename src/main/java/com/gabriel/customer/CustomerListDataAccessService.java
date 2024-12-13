fee94769e2a72e2922f84f787c56cbb7cd570449
Gabrielhs20 <gabo20hernandez@gmail.com>
2024-12-13 00:10:57 -0500

    Removed the demo data from CustomerListDataAccessService, as it was used only before connecting to the database

--- src/main/java/com/gabriel/customer/CustomerListDataAccessService.java
+++ src/main/java/com/gabriel/customer/CustomerListDataAccessService.java
@@ -8,27 +8,8 @@ import java.util.Optional;
 
 @Repository("list")
 public class CustomerListDataAccessService implements CustomerDao{
-    //Database
+    //List of customers
     private static List<Customer>customers;
-
-    static{
-        customers = new ArrayList<>();
-
-        Customer isa = new Customer(
-                2,
-                "Isa",
-                "isa123@gmail.com",
-                23
-        );
-        Customer gabriel = new Customer(
-                1,
-                "Gabriel",
-                "gabo20hernandez@gmail.com",
-                25
-        );
-        customers.add(gabriel);
-        customers.add(isa);
-    }
 
     @Override
     public List<Customer> selectAllCustomers() {
